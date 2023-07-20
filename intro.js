faacb9f8ac9d0028474aec276fd91c50fb70e518
satish998 <100787234+satish998@users.noreply.github.com>
2023-07-20 23:00:13 +0530

    10 lecture assignmet

---
+++ intro.js
@@ -0,0 +1,11 @@
+console.log("this is intro part");
+
+// document.getElementById("heading").innerHTML = "content has been changed";
+function square(num){
+    return num*num;
+}
+square(2);
+var a=2;
+var b="satish";
+var c=["hello",1,2.5];
+console.log(a,b,c)
